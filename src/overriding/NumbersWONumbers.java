0ae66aac572b5811dce4913c4b8c676ffdcd57da
samraj2192 <samraj.bhowmik@gmail.com>
2021-03-14 15:51:51 +0530

    Added numbers without numbers

---
+++ src/overriding/NumbersWONumbers.java
@@ -0,0 +1,12 @@
+package overriding;
+
+public class NumbersWONumbers {
+	public static void main(String[] args) {
+		int a = 'A'/'A';
+		String s = "abscfdrtgf";
+		 for(int i=a; i<=(s.length())*(s.length()); i++) {
+			 System.out.println(i);
+		 }
+	}
+
+}
