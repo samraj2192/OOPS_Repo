b9acf6bbe8d7f6d1449b2ca11b76d12980460506
samraj2192 <samraj.bhowmik@gmail.com>
2021-03-20 17:28:33 +0530

    added numbers by zero

---
+++ src/overriding/NubersbyZero.java
@@ -0,0 +1,15 @@
+package overriding;
+
+public class NubersbyZero {
+
+	public static void main(String[] args) {
+		System.out.println(9.0/0);
+		//System.out.println(9/0);
+		System.out.println(9.000f/0);
+		System.out.println(9.9999d/0);
+		//System.out.println(0/0);
+		System.out.println(0.0/0);
+		System.out.println(0.0/0.0);
+	}
+
+}
